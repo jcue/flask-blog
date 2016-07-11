c80e60eb312363829242ebbc49f8778857a613b5
Jose Cuellar <cuellar_jose@hotmail.com>
2016-07-10 23:37:05 -0700

    Added a login form

--- blog.py
+++ blog.py
@@ -8,8 +8,12 @@ import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = '\x0c\x08Z>\xeaZ\xb8N/~g?4.=b\xb5\xc6\x1a\xb2E\xab\xb4b'
 
 app = Flask(__name__)
+
 
 # pulls in app configuration by looking for UPPERCASE variables
 app.config.from_object(__name__)
@@ -18,13 +22,27 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+            request.form['password'] != app.config['PASSWORD']:
+                error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
 
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
